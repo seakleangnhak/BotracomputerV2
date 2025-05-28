05cb7633577b2edbfe6cf39b612719f07b8f0068
seakleang.nhak <seakleang.nhak@kesspay.io>
2025-05-28 13:56:18 +0700

    add tailwind, partytown and unipic

--- src/root.tsx
+++ src/root.tsx
@@ -1,5 +1,6 @@
 import { component$, isDev } from "@builder.io/qwik";
 import { QwikCityProvider, RouterOutlet } from "@builder.io/qwik-city";
+import { QwikPartytown } from './components/partytown/partytown';
 import { RouterHead } from "./components/router-head/router-head";
 
 import "./global.css";
@@ -12,10 +13,22 @@ export default component$(() => {
    * Don't remove the `<head>` and `<body>` elements.
    */
 
+  const partytownScript = `
+            window.dataLayer = window.dataLayer || [];
+            window.gtag = function() {
+              dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+            gtag('config', 'G-7XL1BKJ430');
+          `
+
   return (
     <QwikCityProvider>
       <head>
         <meta charset="utf-8" />
+        <QwikPartytown forward={['gtag','dataLayer.push']} />
+        <script async type="text/partytown" src="https://www.googletagmanager.com/gtag/js?id=G-7XL1BKJ430"/>
+        <script type="text/partytown" dangerouslySetInnerHTML={partytownScript}/>
         {!isDev && (
           <link
             rel="manifest"
