ac3395cd2fc06333a7ac3d951814adb64b5237e0
seakleang.nhak <seakleang.nhak@kesspay.io>
2025-06-18 11:53:28 +0700

    add netlify

--- src/entry.vercel-edge.tsx
+++ src/entry.vercel-edge.tsx
@@ -7,15 +7,15 @@
  * - https://qwik.dev/docs/deployments/vercel-edge/
  *
  */
-import {
-  createQwikCity,
-  type PlatformVercel,
-} from "@builder.io/qwik-city/middleware/vercel-edge";
-import qwikCityPlan from "@qwik-city-plan";
-import render from "./entry.ssr";
+// import {
+//   createQwikCity,
+//   type PlatformVercel,
+// } from "@builder.io/qwik-city/middleware/vercel-edge";
+// import qwikCityPlan from "@qwik-city-plan";
+// import render from "./entry.ssr";
 
-declare global {
-  type QwikCityPlatform = PlatformVercel
-}
+// declare global {
+//   type QwikCityPlatform = PlatformVercel
+// }
 
-export default createQwikCity({ render, qwikCityPlan });
+// export default createQwikCity({ render, qwikCityPlan });
